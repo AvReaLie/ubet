559136be94617ee01a9fe4ac46aeaf06c6d3ebfe
archaid <62365449+archaid@users.noreply.github.com>
2020-12-15 18:40:04 +0700

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -1,10 +1,10 @@
 <?php
 session_start(); 
-//require '../utils/config.php';
-/*if(!isset($_SESSION['log_adm'])){
+require '../utils/config.php';
+if(!isset($_SESSION['log_adm'])){
 echo "<script>alert('Login terlebih dahulu');document.location='../'</script>";
   exit();
-  }*/
+  }
 
 date_default_timezone_set('Asia/Jakarta');
 
@@ -15,11 +15,11 @@ switch($page){
   require 'view/404.php';
   break; 
 
-/*case 'logout':
+case 'logout':
         unset($_SESSION['log_adm']);
         //session_destroy();
         echo "<script>alert('$_SESSION[adm_name] Berhasil Logout');document.location='$admin_url';</script>";
-        break;*/
+        break;
 
   case 'dashboard':
   $judul = "Ubet | Dashboard";  
@@ -28,11 +28,6 @@ switch($page){
   require 'view/body.php';
   require 'view/f.php';
   break;
-
-  /*case 'daftar':
-  $judul = "Ubet | Daftar";  
-  require 'view/signup.php';
-  break; */ 
 
 break;  
 }
