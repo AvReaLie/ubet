c5c041b3d4089d0f4a90c6678377c222c385e9f7
archaid <62365449+archaid@users.noreply.github.com>
2020-12-21 22:59:44 +0700

    Update h.php

--- admin/view/h.php
+++ admin/view/h.php
@@ -39,5 +39,28 @@
 			100%{-webkit-transform:scale(1);transform:scale(1);opacity:0}
 		}
 	</style>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 	<link href="assets/css/style.css" rel="stylesheet">
+	<link href="assets/sweetalert2/sweetalert2.min.css" rel="stylesheet">
+	<link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" rel="stylesheet">
+
+	<style type="text/css">
+		.error-msg{
+			color: #dc3545;
+			font-weight: 600;
+		}
+		.success-msg{
+			color: green;
+			font-weight: 600;
+		}
+		.field-icon {
+          float: right;
+          margin-left: -30px;
+          margin-top: -26px;
+          margin-right: 10px;
+          position: relative;
+          z-index: 2;
+        }
+	</style>
 </head>
